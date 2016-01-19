5dce076a59503495330caccd0730953bbb3047fb
yesterday17 <huaercz@163.com>
2016-01-19 18:52:51 +0800

    基本数据类型完成。

--- DanmakuRank/Class1.cs
+++ DanmakuRank/Class1.cs
@@ -5,12 +5,14 @@ using System.Text;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Xml;
+using System.IO;
 
 namespace DanmakuRank
 {
     public class MainClass : BilibiliDM_PluginFramework.DMPlugin
     {
-        public static string configFileLocation = Environment.SpecialFolder.MyDocuments + "\弹幕姬\Config\countress.xml";
+        public static string configFileLocation = Environment.SpecialFolder.MyDocuments + "\\弹幕姬\\Config\\countress.xml";
+        public static Data _data = new Data();
 
         public MainClass()
         {
@@ -23,7 +25,23 @@ namespace DanmakuRank
 
         private void Class1_ReceivedDanmaku(object sender, BilibiliDM_PluginFramework.ReceivedDanmakuArgs e)
         {
-            //throw new NotImplementedException();
+            //首先分析rawData中cmd是否等于DANMU_MSG然后进行操作
+            string cmd_tmp = e.Danmaku.RawData.Substring(e.Danmaku.RawData.IndexOf("\"cmd\"") + 7);
+            string cmd = cmd_tmp.Substring(0, cmd_tmp.IndexOf("\""));
+
+
+            if (cmd == "DANMU_MSG")
+            {
+                if (_data.getPersonByName(e.Danmaku.CommentUser) != null)
+                {
+                    _data.getPersonByName(e.Danmaku.CommentUser).AddDanmaku(e.Danmaku);
+                }
+                else
+                {
+                    _data.AddPerson(e.Danmaku);
+                }
+                //System.Windows.Forms.MessageBox.Show(e.Danmaku.CommentText + "——by " + e.Danmaku.CommentUser);
+            }
         }
 
         public override void Admin()
@@ -34,13 +52,21 @@ namespace DanmakuRank
 
         public override void Stop()
         {
-            AddDM("统计姬已停止。");
+            //AddDM("统计姬已停止。");   
             base.Stop();
         }
 
         public override void Start()
         {
-            AddDM("统计姬已开始为您效劳。");
+            //这个有些不大对劲= =是新开了一个window然后发弹幕的= =
+            Bililive_dm.MainWindow x = new Bililive_dm.MainWindow();
+            x.AddDMText("123", "456");
+
+            Bililive_dm.MainOverlay y = x.overlay;
+
+            y.Hide();
+            //x.Left = 10000;
+            //x.Top = 10000;
             base.Start();
         }
     }
---
+++ DanmakuRank/Data.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanmakuRank
+{
+    public class Data
+    {
+        public class Person
+        {
+            public Person()
+            {
+                this.name = null;
+                this.id = 0;
+                this.id_s = id.ToString();
+                this.isAdmin = false;
+                this.isVip = false;
+                this.level = -1;
+                this.rank = null;
+
+                this.danmaku = new List<BilibiliDM_PluginFramework.DanmakuModel>();
+            }
+
+            public Person(string u_name, int u_id, bool u_isAdmin, bool u_isVip, int u_level, string u_rank)
+            {
+                this.name = u_name;
+                this.id = u_id;
+                this.id_s = u_id.ToString();
+                this.isAdmin = u_isAdmin;
+                this.isVip = u_isVip;
+                this.level = u_level;
+                this.rank = u_rank;
+                this.danmaku = new List<BilibiliDM_PluginFramework.DanmakuModel>();
+            }
+
+            public Person(int u_id)
+            {
+                //暂时不实现，从bilibili服务器上获取信息
+            }
+
+            public Person(string rawData)
+            {
+                this.name = "";
+                this.id = 0;
+                this.id_s = "";
+                this.isAdmin = false;
+                this.isVip = false;
+                this.level = -1;
+                this.rank = "";
+
+                this.danmaku = new List<BilibiliDM_PluginFramework.DanmakuModel>();
+            }
+
+            public Person(BilibiliDM_PluginFramework.DanmakuModel d)
+            {
+                this.name = d.CommentUser;
+                this.id = Convert.ToInt32(getJson("\",[", ",\"", d.RawData));
+                this.id_s = id.ToString();
+                this.isAdmin = d.isAdmin;
+                this.isVip = d.isVIP;
+                this.level = -1;
+                this.rank = "";
+
+                this.danmaku = new List<BilibiliDM_PluginFramework.DanmakuModel>();
+            }
+
+            public void update(string u_name, bool u_isAdmin, bool u_isVip, string u_rank)
+            {
+                this.name = u_name;
+                this.isAdmin = u_isAdmin;
+                this.isVip = u_isVip;
+                this.rank = u_rank;
+            }
+
+            public string Name
+            {
+                get { return this.name; }
+                set { this.name = value; }
+            }
+
+            public int Id
+            {
+                get { return this.id; }
+                set { this.id = value; }
+            }
+
+            public string ID
+            {
+                get { return this.id_s; }
+                set { this.id_s = value; }
+            }
+
+            public bool Admin
+            {
+                get { return this.isAdmin; }
+                set { this.isAdmin = value; }
+            }
+
+            public bool Vip
+            {
+                get { return this.isVip; }
+                set { this.isVip = value; }
+            }
+
+            public int Level
+            {
+                get { return this.level; }
+                set { this.level = value; }
+            }
+
+            public string Rank
+            {
+                get{ return this.rank; }
+                set { this.rank = value; }
+            }
+
+            private string name;
+            private int id;
+            private string id_s;
+            private bool isAdmin;
+            private bool isVip;
+            private int level;
+            private string rank;
+            private List<BilibiliDM_PluginFramework.DanmakuModel> danmaku;
+
+
+            public void AddDanmaku(BilibiliDM_PluginFramework.DanmakuModel dm)
+            {
+                danmaku.Add(dm);
+            }
+
+            public int getDanmakuNum()
+            {
+                return danmaku.Count(); 
+            }
+
+            /// <summary>
+            /// 获取制定Json中的值信息
+            /// </summary>
+            /// <param name="key">开头的关键字</param>
+            /// <param name="key_end">结束的关键字</param>
+            /// <param name="json">Json原文</param>
+            /// <returns></returns>
+            private string getJson(string key, string key_end, string json)
+            {
+                string tmp = json.Substring(json.IndexOf(key) + key.Length);
+                string ans = tmp.Substring(0, tmp.IndexOf(key_end)- key_end.Length + 2);
+                return ans;
+            }
+
+
+        }
+
+        private List<Person> dt = new List<Person>();
+
+        public void AddPerson(Person x)
+        {
+            //Person x = new Person(name, id, isAdmin, isVip, level, rank);
+            dt.Add(x);
+        }
+
+        public void AddPerson(BilibiliDM_PluginFramework.DanmakuModel x)
+        {
+            dt.Add(new Person(x));
+            this.getPersonByName(x.CommentUser).AddDanmaku(x);
+        }
+
+        public Person getPersonByName(string u_name)
+        {
+            //Person x = new Person(uid);
+            //dt.Find((Person y)=>y.Name==u_name);
+            //return y;
+            return dt.Find(
+                delegate(Person y)
+                {
+                    return y.Name == u_name;
+                }
+                );
+        }
+
+        public Person getPersonById(int u_id)
+        {
+            return dt.Find((Person y) => y.Id == u_id);
+        }
+
+        public Person getPersonById(string u_id)
+        {
+            return dt.Find((Person y) => y.ID == u_id);
+        }
+
+        public void editByPerson(Person old, Person _new)
+        {
+            Person f = dt.Find((Person y)=>y==old);
+
+        }
+
+        public object getType()
+        {
+            return dt.GetType();
+        }
+
+    }
+}
